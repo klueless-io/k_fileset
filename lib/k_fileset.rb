57c28b7aba50148153fa08021646a647afc8bce6
David Cruwys <david@ideasmen.com.au>
2022-06-29 14:58:06 +1000

    update rubocop.yml

--- lib/k_fileset.rb
+++ lib/k_fileset.rb
@@ -16,7 +16,7 @@ module KFileset
   # Your code goes here...
 end
 
-if ENV['KLUE_DEBUG']&.to_s&.downcase == 'true'
+if ENV.fetch('KLUE_DEBUG', 'false').downcase == 'true'
   namespace = 'KFileset::Version'
   file_path = $LOADED_FEATURES.find { |f| f.include?('k_fileset/version') }
   version = KFileset::VERSION.ljust(9)
